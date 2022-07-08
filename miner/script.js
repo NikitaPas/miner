f00fb5071e1b8625f4db7440820d33e02093cc58
Nikita Pasiukov <nikita223372@gmail.com>
2022-07-08 14:23:04 +0300

    alpha version

---
+++ miner/script.js
@@ -0,0 +1,11 @@
+var name;
+var balance;
+
+var startBtn = document.getElementById("submit");
+startBtn.onclick = function(){
+    name = document.start.nick.value;
+    balance = document.start.bal.value;
+}
+
+console.log(name);
+console.log(balance);
